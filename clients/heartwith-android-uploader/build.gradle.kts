356f331e59530943384d8be9db2b3f6a2450f09b
jizizr <jizizr1234@gmail.com>
2026-06-01 03:02:54 +0800

    声明上传 SDK 的 Gradle 依赖坐标

--- clients/heartwith-android-uploader/build.gradle.kts
+++ clients/heartwith-android-uploader/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.library)
 }
 
+group = "com.heartwith"
+version = findProperty("heartwithClientVersionName") as String? ?: "1.0.0"
+
 android {
     namespace = "com.heartwith.uploader"
     compileSdk = 37
